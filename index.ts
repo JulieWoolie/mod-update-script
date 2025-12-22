93f453d36645c0060aea220e14e7afee92028459
JulieWoolie <andero.magi.420@gmail.com>
2025-12-22 15:56:09 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,239 @@
+import * as npath from "node:path"
+import * as https from "node:https"
+import * as fs from "node:fs"
+import { parseArgs } from "jsr:@std/cli/parse-args";
+
+export {}
+
+interface Params {
+  version: string
+  loader: string
+  modlistFile: string
+  outputDir: string
+}
+
+interface ModEntry {
+  id: string,
+  name: string
+}
+
+interface DownloadEntrySuccess {
+  type: "found"
+  mod: ModEntry
+  downloadUrl: string
+  filename: string
+}
+
+interface DownloadEntryError {
+  type: "err"
+  mod: ModEntry
+  message: string
+}
+
+type DownloadEntry = DownloadEntryError | DownloadEntrySuccess
+
+const LOADER = `fabric`
+const VERSION = `1.21.11`
+const MODLIST_FILE = "./modlist.txt"
+const OUTPUT_DIR = `..`
+const API_BASE_URL = `https://api.modrinth.com/v2`
+
+main()
+
+async function main() {
+  const params: Params = setupParams()
+
+  console.log(`Finding updated mods for ${params.loader}, version: ${params.version}`)
+  console.log(`modlist file: ${params.modlistFile}, output directory: ${params.outputDir}`)
+
+  if (!fileExists(params.modlistFile)) {
+    console.log(`[FAILED ] Modlist file (${params.modlistFile}) does not exist`)
+    return
+  }
+
+  let modlist: ModEntry[]
+  try {
+    modlist = await loadModList(params.modlistFile)
+  } catch (err) {
+    console.log(`[FAILED ] Error loading modlist txt file`, err)
+    return
+  }
+
+  let entries: DownloadEntry[]
+  try {
+    entries = await findUrlsAndVersions(params, modlist)
+  } catch (err) {
+    console.log(`[FAILED ] Error getting Mod URLs and versions`, err)
+    return
+  }
+
+  await downloadEntries(entries, params)
+}
+
+async function downloadEntries(entries: DownloadEntry[], params: Params) {
+  let failed = 0
+  let succeeded = 0
+  let toDownloadCount = 0
+
+  for (const e of entries!) {
+    if (e.type == "err") {
+      console.log(`[FAILED ] ${e.mod.name} failed to find: ${e.message}`)
+      failed++
+    } else {
+      const outPath = `${params.outputDir}/${e.filename}`
+      if (!fileExists(outPath)) {
+        console.log(`[SUCCESS] ${e.mod.name} found. filename = ${e.filename}`)
+        toDownloadCount++
+      }
+      succeeded++
+    }
+  }
+
+  console.log(`Found ${succeeded} mods, failed to find ${failed} mods, total: ${entries.length}`)
+
+  if (toDownloadCount > 0) {
+    console.log("Downloading...")
+  } else {
+    return
+  }
+
+  for (const entry of entries) {
+    if (entry.type == "err") {
+      continue
+    }
+
+    const outPath = `${params.outputDir}/${entry.filename}`
+    if (fileExists(outPath)) {
+      continue
+    }
+
+    try {
+      await download(entry.downloadUrl, outPath)
+      console.log(`[SUCCESS] Downloaded ${entry.filename}`)
+    } catch (err) {
+      console.log(`Failed to download ${entry.filename} (mod = ${entry.mod.name}):`, err)
+    }
+  }
+}
+
+async function findUrlsAndVersions(params: Params, modlist: ModEntry[]): Promise<DownloadEntry[]> {
+  const arr: DownloadEntry[] = []
+
+  for (const mod of modlist) {
+    const apiUrl = `${API_BASE_URL}/project/${mod.id}/version?loaders=["${params.loader}"]&game_versions=["${params.version}"]`
+
+    try {
+      const result = await findModVersion(apiUrl, mod, params)
+      arr.push(result)
+    } catch (exc) {
+      const err: DownloadEntryError = {
+        type: "err",
+        message: "Failed to fetch: " + exc,
+        mod: mod
+      }
+      arr.push(err)
+    }
+  }
+
+  return arr
+}
+
+async function findModVersion(apiUrl: string, mod: ModEntry, params: Params): Promise<DownloadEntry> {
+  const response = await fetch(apiUrl)
+  const json = await response.json()
+
+  if (json.length < 1) {
+    return {
+      type: "err",
+      message: `Unable to find ${params.loader} mod for version ${params.version}`,
+      mod
+    }
+  }
+
+  const versionData = json[0];
+  const file = versionData.files[0]
+
+  return {
+    type: "found",
+    mod,
+    downloadUrl: file.url,
+    filename: file.filename
+  }
+}
+
+function fileExists(path: string): boolean {
+  return fs.existsSync(path)
+}
+
+async function download(url: string, outputPath: string): Promise<void> {
+  const dirname = npath.dirname(outputPath)
+  if (!fileExists(dirname)) {
+    fs.mkdirSync(dirname)
+  }
+
+  const stream = fs.createWriteStream(outputPath)
+
+  return new Promise((res, rej) => {
+    https.get(url, response => {
+      if (response.statusCode !== 200) {
+        rej(`Failed to get ${url} (${response.statusCode})`)
+        return
+      }
+
+      response.pipe(stream)
+
+      stream.on('finish', () => {
+        stream.close()
+        res()
+      })
+
+      stream.on('error', (err) => {
+        fs.unlinkSync(outputPath)
+        rej(err)
+      })
+    }).on('error', rej)
+  })
+}
+
+function setupParams(): Params {
+  const args = Deno.args
+
+  const parsed: any = parseArgs(Deno.args, {
+    string: ["loader", "version", "modlist-file", "output-dir"],
+    default: {
+      "loader": LOADER,
+      "version": VERSION,
+      "modlist-file": MODLIST_FILE,
+      "output-dir": OUTPUT_DIR
+    }
+  })
+
+  return {
+    loader: parsed.loader,
+    version: parsed.version,
+    modlistFile: tryResolveRealpath(parsed["modlist-file"]),
+    outputDir: tryResolveRealpath(parsed["output-dir"]),
+  }
+}
+
+function tryResolveRealpath(path: string): string {
+  try {
+    return fs.realpathSync(path)
+  } catch (err) {
+    return path
+  }
+}
+
+async function loadModList(fname: string): Promise<ModEntry[]> {
+  const arr: ModEntry[] = []
+  const str: string = fs.readFileSync(fname).toString()
+
+  const split: string[] = str.split(/[\r\n]+/)
+
+  for (const kv of split) {
+    const [id, name] = kv.split(/\s+/)
+    arr.push({id, name})
+  }
+
+  return arr
+}
